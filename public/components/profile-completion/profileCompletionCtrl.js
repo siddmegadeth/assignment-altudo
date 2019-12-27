f2919f15b2af3245fb2983cfd14003b3cb26f98d
Siddharth Chandra <Siddharth.Chandra@gep.com>
2019-12-27 22:26:21 +0530

    Status

--- public/components/profile-completion/profileCompletionCtrl.js
+++ public/components/profile-completion/profileCompletionCtrl.js
@@ -3,12 +3,32 @@ app.controller("profileCompletionCtrl", ['$scope', '$timeout', function($scope, 
 
 	$scope.updateProfile = function(fullName) {
 
-		if (fullName == undefined) {
+		log(fullName);
+		log(fullName.length);
+		if (fullName == undefined || fullName.length == 0) {
 			ons.notification.toast({
 				message: 'Please Provide Your Name',
 				timeout: 2000,
 				buttonLabel: 'Dismiss'
 			});
+		} else {
+
+			// create logic to update user profile
+			var profile = {};
+			profile.full_name = fullName;
+			profile.isProfileCompleted = true;
+
+			ons.notification.toast({
+				message: 'updating......',
+				timeout: 2000,
+				buttonLabel: 'Dismiss'
+			}).then(function()
+			{
+				log("Updating....");
+
+			});
+
+
 		}
 	}
 
